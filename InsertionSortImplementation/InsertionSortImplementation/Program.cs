7704c42fc48d5f9eea60877357b8c43393076a88
Somaya Mohamed <118680203+somiamohamed@users.noreply.github.com>
2024-04-21 20:03:43 +0200

    Add files via upload

---
+++ InsertionSortImplementation/InsertionSortImplementation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 5, 3, 8, 4, 2 };
+        InsertionSort(arr);
+
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static void InsertionSort<T>(T[] arr, IComparer<T> comparer = null) where T : IComparable
+    {
+        if (comparer == null)
+        {
+            comparer = Comparer<T>.Default;
+        }
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            T key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && comparer.Compare(arr[j], key) > 0)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}
